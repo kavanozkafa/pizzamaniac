23f59f1313734ea23639d0660a38e5ea0b23ef4a
Şammas Çölkesen <sammascolkesen@gmail.com>
2019-08-02 22:04:47 +0300

    Add files via upload
    template

---
+++ src/main/java/pizze/PizzamaniacApplication.java
@@ -0,0 +1,13 @@
+package pizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamaniacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamaniacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pizze/PizzamaniacApplicationTests.java
@@ -0,0 +1,16 @@
+package pizze;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PizzamaniacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
